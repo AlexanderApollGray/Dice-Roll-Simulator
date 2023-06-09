929e133675f6a9e0a6f4cd67b85e1128eb36f055
AlexanderApollGray <90724200+AlexanderApollGray@users.noreply.github.com>
2023-06-09 10:23:05 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿// Dice Roll Simulator by Xander
+Console.Clear();
+#nullable disable
+
+// Title
+Console.WriteLine("Dice Roll Simulator Menu");
+
+// Main Loop
+while (true)
+{
+
+    // Dice Results
+    Random rnd = new Random();
+
+    // Menu
+    Console.WriteLine("\n1. Roll Dice Once");
+    Console.WriteLine("2. Roll Dice 5 Times");
+    Console.WriteLine("3. Roll Dice 'n' Times");
+    Console.WriteLine("4. Roll Dice Until Snake Eyes");
+    Console.WriteLine("5. Exit");
+    // Get Input from User
+    Console.Write("Select an option (1-5): ");
+    string option = Console.ReadLine();
+
+    // Process Input
+    if (option == "1")
+    {
+        int dice1 = rnd.Next(1, 7);
+        int dice2 = rnd.Next(1, 7);
+        int sum = dice1 + dice2;
+        Console.WriteLine($"{dice1}, {dice2} (sum: {sum})");
+    }
+    else if (option == "2")
+    {
+
+        for (int n = 0; n < 5; n++)
+        {
+            int dice1 = rnd.Next(1, 7);
+            int dice2 = rnd.Next(1, 7);
+            int sum = dice1 + dice2;
+            Console.WriteLine($"{dice1}, {dice2} (sum: {sum})");
+        }
+    }
+    else if (option == "3")
+    {
+        Console.Write("\nHow many rolls would you like? ");
+            int number = Convert.ToInt16(Console.ReadLine());
+            for (int n = 0; n < number; n++) {
+                int dice1 = rnd.Next(1, 7);
+                int dice2 = rnd.Next(1, 7);
+                int sum = dice1 + dice2;
+                Console.WriteLine($"{dice1}, {dice2} (sum: {sum})");
+            }
+    }
+    else if (option == "4")
+    {
+        for (int n = 0; n < 1;)
+        {
+            int dice1 = rnd.Next(1, 7);
+            int dice2 = rnd.Next(1, 7);
+            int sum = dice1 + dice2;
+            Console.WriteLine($"{dice1}, {dice2} (sum: {sum})");
+            if (sum == 2)
+            {
+                n++;
+            }
+        }
+    }
+    else if (option == "5")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid Entry");
+    }
+}
